d864c7b8c46fc91126241f63bcb0065def859ccb
ZaneleNkosi <50693705+ZaneleNkosi@users.noreply.github.com>
2019-08-23 15:05:29 +0200

    changes

--- src/app/bookwithsalon/bookwithsalon.page.ts
+++ src/app/bookwithsalon/bookwithsalon.page.ts
@@ -298,42 +298,22 @@ async presentAlertConfirm() {
 testarray=[];
 testbooking(booking)
 {
-
-
-  let hourRange = parseFloat(booking.sessiontime[0]+booking.sessiontime[1]);
+ let hourRange = parseFloat(booking.sessiontime[0]+booking.sessiontime[1]);
 let minuteRange =parseFloat(booking.sessiontime[3]+booking.sessiontime[4])
 console.log((minuteRange));
- this.backend.db.collection('SalonNode').doc('Nakanjani').collection('staff').doc('busi').collection('2019-8-23').get().then(val=>{
- val.forEach(doc=>{
-   
-   this.testarray.push(doc.data());
-  
-  })
-  console.log(this.testarray)
+this.backend.db.collection('SalonNode').doc('Nakanjani').collection('staff').doc('busi').collection('2019-8-23').get().then(val=>{
+val.forEach(doc=>{
+  this.testarray.push(doc.data());
  })
-
- for(let i =0;i<this.testarray.length;i++)
- {
-if(parseFloat(this.testarray[i].sessiontime[0]+this.testarray[i].sessiontime[1])==hourRange)
- {
- console.log("Got Something")
- this.control.SlotToast();
- } 
-
+ console.log(this.testarray)
+})
 for(let i =0;i<this.testarray.length;i++)
 {
-
-  console.log("sesyarray and usertime",parseFloat(this.testarray[i].sessiontime[0]+this.testarray[i].sessiontime[1]),"usertime",hourRange)
-if(parseFloat(this.testarray[i].sessiontime[0]+this.testarray[i].sessiontime[1])==hourRange )
+if(parseFloat(this.testarray[i].sessiontime[0]+this.testarray[i].sessiontime[1])==hourRange)
 {
 console.log("Got Something")
 this.control.SlotToast();
-} 
-else{
-  this.control.SlotToast1();
-} 
 }
 }
-
 }
 }
