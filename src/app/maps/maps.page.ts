bca629ea5ced022bbf81dc205df1a9efdade865c
seko17 <34337236+seko17@users.noreply.github.com>
2019-09-03 13:51:33 +0200

    siseko

--- src/app/maps/maps.page.ts
+++ src/app/maps/maps.page.ts
@@ -1,9 +1,13 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, NgZone } from '@angular/core';
 import { AlertController, LoadingController } from '@ionic/angular';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { NativeGeocoder, NativeGeocoderOptions, NativeGeocoderResult } from '@ionic-native/native-geocoder/ngx';
 import * as firebase from 'firebase';
 import { snapshotChanges } from '@angular/fire/database';
+import { Button } from 'protractor';
+import { Router } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+import {Marker, Geocoder} from '@ionic-native/google-maps';
 declare var google
 @Component({
   selector: 'app-maps',
@@ -15,13 +19,21 @@ export class MapsPage implements OnInit {
 @ViewChild("map", { static: false }) mapElement: ElementRef ;
 infoWindow = new google.maps.InfoWindow;
 map: any;
+private googleAutoComplete = new google.maps.places.AutocompleteService();
+public searchResults = new Array<any>();
+public search : string ='';
+private originMarker : Marker;
+public destination : any;
+ geocoder = new google.maps.Geocoder;
 bounds = {
   north: -21.914461,
   south: -35.800139,
   west: 15.905430,
   east: 34.899504
 }
+
 users = []
+mark = []
 mapCenter = {
   lat: 0,
   lng: 0
@@ -31,42 +43,67 @@ db = firebase.firestore();
 geoAccuracy: number;
 geoAddress: string;
 showAutoHideLoader : any;
-  constructor(public alertCtrl: AlertController, private geolocation: Geolocation,public loadingController: LoadingController) {
+  salonName: any;
+  sname: any;
+  constructor(public alertCtrl: AlertController, private geolocation: Geolocation,public loadingController: LoadingController, private router : Router,private ngZone : NgZone) {
+console.log(google);
 
     this.db.collection('SalonNode').onSnapshot(snapshot => {
       snapshot.forEach(doc => {
    
         this.users.push(doc.data());
         console.log('Retrive messege:',this.users); 
-        //this.addMarkersOnTheCustomersCurrentLocation();
-
         this.users.forEach(Customers => {
-      this.addMarkersOnTheCustomersCurrentLocation(Customers.coords.lat, Customers.coords.lng);
+          const icon = {
+            url: '../../assets/icon/salon.png', // image url
+            scaledSize: new google.maps.Size(50, 50), // scaled size
+            origin: new google.maps.Point(0,0), // origin
+            anchor: new google.maps.Point(0, 0) // anchor
+          };
+          
+          let marker = new google.maps.Marker({
+              map: this.mapElement,
+              animation: google.maps.Animation.DROP,
+              position: new google.maps.LatLng(Customers.coords.lat, Customers.coords.lng),
+              icon: icon
+              
+          });
+          let content = 'Salon Name' + Customers.salonName + '<br>' + Customers.SalonContactNo + '<br>' + '<button (click)="move()" size="small"  color="secondary">View more</button>'
+          this.addInfoWindow(marker, content);
       // Customers.coords.lat, Customers.coords.lng
       console.log('coordsdddd', Customers);
-      
      }) 
-
-
       })
-
-    
     });
-  
-    // this.users.forEach(Customers => {
-    //   this.addMarkersOnTheCustomersCurrentLocation(Customers.coords.lat, Customers.coords.lng);
-
-    //  }) 
-
-    
-
    }
-
+   
+move(){
+  this.router.navigateByUrl('navigation');
+// this.router.navigate(['navigation']);
+console.log('checkl')
+}
+searchChanged(){
+ if(!this.search.trim().length) return;
+  this.googleAutoComplete.getPlacePredictions({ input: this.search}, predictions =>{
+    //console.log(predictions);
+    this.ngZone.run(() =>{
+      this.searchResults = predictions;
+    });
+  });
+}
+async calcRoute(item:any){
+  this.search = ''  
+  this.destination = item;
+  const info: any = await Geocoder.geocode({address: this.destination.description});
+  console.log('mama',info);
+  
+}
   ngOnInit() {
     // this.addMarkersOnTheCustomersCurrentLocation();
   
   }
   addInfoWindow(marker, content){
+
     let infoWindow = new google.maps.InfoWindow({
       content: content
     });
@@ -75,29 +112,36 @@ showAutoHideLoader : any;
     });
   }
 
-  // addMarkersOnTheCustomersCurrentLocation(lat, lng){
-
-  //   let marker = new google.maps.Marker({
-  //       map: this.map,
-  //       animation: google.maps.Animation.DROP,
-  //       position: new google.maps.LatLng(lat, lng),
-  //   });
-  //   let content = "<p>Customer's Location!</p>";          
-  //   this.addInfoWindow(marker, content);
-  // }
-
-  
+   v : string = 'sdfdfgd';
   addMarkersOnTheCustomersCurrentLocation(lat, lng){
 
-
+    const icon = {
+      url: '../../assets/icon/salon.png', // image url
+      scaledSize: new google.maps.Size(50, 50), // scaled size
+      origin: new google.maps.Point(0,0), // origin
+      anchor: new google.maps.Point(0, 0) // anchor
+    };
     
     let marker = new google.maps.Marker({
         map: this.mapElement,
         animation: google.maps.Animation.DROP,
         position: new google.maps.LatLng(lat, lng),
+        icon: icon
     });
-    let content = "<p>Salon's location!</p>";          
+this.db.collection('SalonNode').get().then(snap =>{
+  snap.forEach(doc =>{
+    this.mark.push(doc.data())
+    this.mark.forEach(Cus =>{
+      this.sname = Cus.salonName;
+    })
+    let content = ' ' +  this.sname 
+
+
     this.addInfoWindow(marker, content);
+  })
+})
+           
+   
     
     console.log('addMarkersOnTheCustomersCurrentLocation called');
   }
@@ -129,24 +173,39 @@ showAutoHideLoader : any;
     }
     getGeolocation(){
       this.geolocation.getCurrentPosition().then((resp) => {
-        // this.geoLatitude = resp.coords.latitude;
-        // this.geoLongitude = resp.coords.longitude; 
-        // this.geoAccuracy = resp.coords.accuracy; 
-        // this.getGeoencoder(this.geoLatitude,this.geoLongitude);
-        console.log(resp);
-        
+        console.log(resp);   
         this.mapCenter.lat = resp.coords.latitude;
         this.mapCenter.lng = resp.coords.longitude;
         this.geoAccuracy = resp.coords.accuracy;
-  
+        const contentString = '<div id="content"> ' +
+        '<div id="siteNotice">' +
+        '</div>' +
+        '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
+        '<div id="bodyContent">' +
+        '<img src="assets/icon/user.png" width="200">' +
+        '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+        'sandstone rock formation in the southern part of the ' +
+        'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) ' +
+        'south west of the nearest large town, Alice Springs; 450&#160;km ' +
+        '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major ' +
+        'features of the Uluru - Kata Tjuta National Park. Uluru is ' +
+        'sacred to the Pitjantjatjara and Yankunytjatjara, the ' +
+        'Aboriginal people of the area. It has many springs, waterholes, ' +
+        'rock caves and ancient paintings. Uluru is listed as a World ' +
+        'Heritage Site.</p>' +
+        '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+        'https://en.wikipedia.org/w/index.php?title=Uluru</a> ' +
+        '(last visited June 22, 2009).</p>' +
+        '</div>' +
+        '</div>';
         const marker = {
           coords: {
             lat: resp.coords.latitude,
           lng: resp.coords.longitude
           },
-          content: 'ME',
-          name: '',
-          //icon: iconBase + 'parking_lot_maps.png'
+          content: contentString,
+          name: 'My Current Location',
+        
         }
 
         const markers = {
@@ -184,7 +243,7 @@ showAutoHideLoader : any;
       return mapOptions;
     }
     initMap(){
-     
+
       this.mapElement = new google.maps.Map(this.mapElement.nativeElement, this.mapOptions());
     }
 
@@ -213,10 +272,16 @@ showAutoHideLoader : any;
 
    
     addMarker(props) {
-  
+      const icon = {
+        url: '../../assets/icon/u.png', // image url
+        scaledSize: new google.maps.Size(50, 50), // scaled size
+        origin: new google.maps.Point(0,0), // origin
+        anchor: new google.maps.Point(0, 0) // anchor
+      };
       const marker = new google.maps.Marker({
         position: props.coords,
         map: this.mapElement,
+        icon : icon
       })
    
       if(props.iconImage) {
