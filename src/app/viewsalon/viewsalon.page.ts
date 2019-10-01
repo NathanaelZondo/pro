c7d04d57f4e83487b2c838fc51c568d43f64c22d
ZaneleNkosi <50693705+ZaneleNkosi@users.noreply.github.com>
2019-10-01 12:29:32 +0200

    hi

--- src/app/viewsalon/viewsalon.page.ts
+++ src/app/viewsalon/viewsalon.page.ts
@@ -12,72 +12,77 @@ import { ThrowStmt } from '@angular/compiler';
   styleUrls: ['./viewsalon.page.scss'],
 })
 export class ViewsalonPage implements OnInit {
-db = firebase.firestore();
+  db = firebase.firestore();
   sliderConfig = {
     spaceBetween: 5,
-    slidesPerView: 1.2
+    slidesPerView: 1.2,
+    centeredSlides: true,
+    watchOverflow: true
   }
-likes;
-total = 0;
-rate = 0
-dummy = []
-userRating = []
+
+  likes;
+  total = 0;
+  rate = 0
+  dummy = []
+  userRating = []
   more = false;
   hair = [];
   viewhair = true;
-  placeholder =this.backend.salonsDisply;
+  placeholder = this.backend.salonsDisply;
   aveg: number;
-  heart : string ="unlike"
-    constructor(public control:ControlsService,public backend:BackendService,public modalController: ModalController) {
+  heart: string = "unlike"
+  
+  constructor(public control: ControlsService, public backend: BackendService, public modalController: ModalController) {
     this.backend.getHairSalon();
 
     //this.gethairstyles(this.gend);
 
     console.log("selectedsalon data", this.salond)
 
-    firebase.firestore().collection('salonAnalytics').doc(this.salond[0].userUID).collection('numbers').get().then(val=>{
-      val.forEach(doc=>{
-        console.log("These are the likes =",doc.data().likes);
-        this.likes =doc.data().likes;
-})})
+    firebase.firestore().collection('salonAnalytics').doc(this.salond[0].userUID).collection('numbers').get().then(val => {
+      val.forEach(doc => {
+        console.log("These are the likes =", doc.data().likes);
+        this.likes = doc.data().likes;
+      })
+    })
 
-   
+
     console.log(this.likes)
 
-this.db.collection('Salons').where("salonName","==",this.backend.salonname).onSnapshot(doc =>{
-  doc.forEach(res =>{
-    this.db.collection('Salons').doc(res.id).collection('ratings').onSnapshot(snap =>{
-      snap.forEach(doc =>{
-        this.userRating.push(doc.data().rating)
-        console.log('users', doc.data().rating);
-        this.total += doc.data().rating;
-console.log(this.total);
-this.dummy.push(doc.data().rating)
+    this.db.collection('Salons').where("salonName", "==", this.backend.salonname).onSnapshot(doc => {
+      doc.forEach(res => {
+        this.db.collection('Salons').doc(res.id).collection('ratings').onSnapshot(snap => {
+          snap.forEach(doc => {
+            this.userRating.push(doc.data().rating)
+            console.log('users', doc.data().rating);
+            this.total += doc.data().rating;
+            console.log(this.total);
+            this.dummy.push(doc.data().rating)
 
+          })
+          this.aveg = this.total / this.dummy.length;
+          console.log('averge', this.aveg);
+        })
       })
-      this.aveg = this.total / this.dummy.length;
-console.log('averge', this.aveg);
     })
-  })
-})
 
 
-// this.db.collection('Salons').doc(this.backend.salonname).collection('ratings').onSnapshot(snap =>{
-// snap.forEach( doc =>{
-//   this.userRating.push(doc.data().rating)
-//   console.log('users', doc.data().rating);
+    // this.db.collection('Salons').doc(this.backend.salonname).collection('ratings').onSnapshot(snap =>{
+    // snap.forEach( doc =>{
+    //   this.userRating.push(doc.data().rating)
+    //   console.log('users', doc.data().rating);
 
-// this.total += doc.data().rating;
-// console.log(this.total);
-// this.dummy.push(doc.data().rating)
+    // this.total += doc.data().rating;
+    // console.log(this.total);
+    // this.dummy.push(doc.data().rating)
 
 
-// })
-// this.aveg = this.total / this.dummy.length;
-// console.log('averge', this.aveg);
+    // })
+    // this.aveg = this.total / this.dummy.length;
+    // console.log('averge', this.aveg);
 
-// })
-console.log('toatl for ratings',this.total)
+    // })
+    console.log('toatl for ratings', this.total)
   }
   salond = this.backend.selectedsalon;
   ngOnInit() {
@@ -98,7 +103,7 @@ console.log('toatl for ratings',this.total)
   gethairstyles(x) {
 
     console.log(this.backend.salonname)
-this.hair =[];
+    this.hair = [];
     let user = this.db.collection('Salons').doc(this.salond[0].userUID).collection('Styles')
 
 
@@ -114,45 +119,44 @@ this.hair =[];
 
   choosehair(x) {
     console.log(x);
-    this.backend.sethairstyledata(x.hairstyleName, x.duration, x.hairstylePrice,x.hairStyleImage);
+    this.backend.sethairstyledata(x.hairstyleName, x.duration, x.hairstylePrice, x.hairStyleImage);
     this.control.router.navigate(['customer']);
   }
 
   viewHair() {
     this.more = !this.more
   }
-  toogleHaert(){
-    if(this.heart == 'unlike'){
+  toogleHaert() {
+    if (this.heart == 'unlike') {
       this.heart = 'like';
-    }else {
+    } else {
       this.heart = 'unlike';
     }
   }
-  like(x)
-  {
+  like(x) {
 
-console.log(x)
+    console.log(x)
 
     let click = 1;
     let v1;
     let docid;
-     
-    x.userUID;
-    firebase.firestore().collection('Analytics').doc(x.userUID).get().then(val=>{
 
-      console.log("numbers = ",val.data())
-      v1 =val.data().numberofviews+click;
-      firebase.firestore().collection('Analytics').doc(x.userUID).set({"numberofviews":v1},{merge: true})
-let smoray =val.data().users;
-console.log("smoray =",val.data().users)
-smoray.push({voteruid:firebase.auth().currentUser.uid});
-    //smoray.push({useruid:firebase.auth().currentUser.uid});
-      firebase.firestore().collection('Analytics').doc(x.userUID).set({numberofviews:val.data().numberofviews,numberoflikes:val.data().numberoflikes+1,usercancel:val.data().usercancel,saloncancel:val.data().saloncancel,allbookings:val.data().allbookings,users:smoray});
+    x.userUID;
+    firebase.firestore().collection('Analytics').doc(x.userUID).get().then(val => {
+
+      console.log("numbers = ", val.data())
+      v1 = val.data().numberofviews + click;
+      firebase.firestore().collection('Analytics').doc(x.userUID).set({ "numberofviews": v1 }, { merge: true })
+      let smoray = val.data().users;
+      console.log("smoray =", val.data().users)
+      smoray.push({ voteruid: firebase.auth().currentUser.uid });
+      //smoray.push({useruid:firebase.auth().currentUser.uid});
+      firebase.firestore().collection('Analytics').doc(x.userUID).set({ numberofviews: val.data().numberofviews, numberoflikes: val.data().numberoflikes + 1, usercancel: val.data().usercancel, saloncancel: val.data().saloncancel, allbookings: val.data().allbookings, users: smoray });
     });
 
   }
 }
 
 
-  
+
 
