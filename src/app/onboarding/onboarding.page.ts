0742049ac9a1383c76688a92c7a57b4152bf4289
ZaneleNkosi <50693705+ZaneleNkosi@users.noreply.github.com>
2019-10-24 09:15:28 +0200

    changes

--- src/app/onboarding/onboarding.page.ts
+++ src/app/onboarding/onboarding.page.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit , ViewChild} from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ControlsService } from '../controls.service';
 import { Storage } from '@ionic/storage';
-import { IonSlides} from '@ionic/angular';
+import { IonSlides } from '@ionic/angular';
 
 @Component({
   selector: 'app-onboarding',
@@ -9,29 +9,50 @@ import { IonSlides} from '@ionic/angular';
   styleUrls: ['./onboarding.page.scss'],
 })
 export class OnboardingPage implements OnInit {
-  @ViewChild('slides', { static: true })  slides: IonSlides;
-
-  constructor(public control:ControlsService,private storage: Storage) { }
+  @ViewChild('slides', { static: true }) slides: IonSlides;
+  show = false;
+  constructor(public control: ControlsService, private storage: Storage) { }
 
   ngOnInit() {
+    this.slides.lockSwipes(true)
   }
-   async signin() {
-     await this.storage.set('tutorialComplete', true);
-    this.control.navCtrl.setDirection('root');
-    this.control.navCtrl.navigateRoot('/login'); 
-}
   
+  async signin() {
+    await this.storage.set('tutorialComplete', true);
+    this.control.navCtrl.setDirection('root');
+    this.control.navCtrl.navigateRoot('/login');
+  }
 
-  nextslides(){
-    this.slides.slideNext();
+
+  nextslides() {
+    if (this.slides.isEnd) {
+      // this.show = false
+      this.slides.lockSwipes(false)
+      this.slides.slideNext(); 
+   
+      this.slides.getActiveIndex().then(index =>{
+        console.log('slid', index );
+        if(index == 2){
+          this.show = true;
+        }
+      })
+    
+
+      this.slides.lockSwipes(true)
+    } else {
+      this.slides.slideNext();
+    } 
   }
- 
-   async signup() {
-     await this.storage.set('tutorialComplete', true);
+
+  async signup() {
+    await this.storage.set('tutorialComplete', true);
     this.control.navCtrl.setDirection('root');
-    this.control.navCtrl.navigateRoot('/signup'); 
-}
-  
+    this.control.navCtrl.navigateRoot('/signup');
+  }
+
+
+
+
 
 
 
